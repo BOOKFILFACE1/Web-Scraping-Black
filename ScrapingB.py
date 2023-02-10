caf9b77d8ae103216d50f87e6b99fd5cf852c7a9
BOOKFILFACE1 <115002506+BOOKFILFACE1@users.noreply.github.com>
2023-02-09 20:38:22 -0400

    Create ScrapingB.py

---
+++ ScrapingB.py
@@ -0,0 +1,49 @@
+# Importar las librerías necesarias
+import requests
+import random
+import time
+from bs4 import BeautifulSoup
+
+# Función para realizar la solicitud GET a la URL y obtener la página HTML
+def get_html(url):
+    # Agregar un tiempo de espera aleatorio para evitar ser bloqueado por el servidor
+    time.sleep(random.uniform(1, 5))
+    # Enviar una solicitud GET a la URL especificada
+    response = requests.get(url)
+    # Verificar si la respuesta es exitosa
+    if response.status_code == 200:
+        # Devolver el código HTML de la página
+        return response.text
+    else:
+        # Devolver una cadena vacía si la respuesta no es exitosa
+        return ""
+
+# Función para extraer los datos necesarios de la página HTML
+def extract_data(html):
+    # Crear un objeto BeautifulSoup a partir de la página HTML
+    soup = BeautifulSoup(html, 'html.parser')
+    # Buscar todos los elementos con la clase especificada
+    items = soup.find_all(class_="item")
+    # Inicializar una lista para almacenar los datos extraídos
+    data = []
+    # Iterar sobre cada elemento
+    for item in items:
+        # Extraer el título y el precio de cada elemento
+        title = item.find(class_="title").text.strip()
+        price = item.find(class_="price").text.strip()
+        # Agregar un diccionario con los datos extraídos a la lista
+        data.append({"title": title, "price": price})
+    # Devolver la lista de datos extraídos
+    return data
+
+# URL a la que se realizará la solicitud GET
+url = "https://www.example.com/items"
+
+# Obtener la página HTML
+html = get_html(url)
+
+# Extraer los datos necesarios de la página HTML
+data = extract_data(html)
+
+# Imprimir los datos extraídos
+print(data)
